82822012c4578346f52e9577e9b4af2b6bb2df6a
gl1tchs <berkeumtbiricik@gmail.com>
2023-11-08 23:45:59 +0300

    initial commit.

---
+++ editor/cmd/main.cc
@@ -0,0 +1,8 @@
+// Copyright 2023 Berke Umut Biricik All Rights Reserved
+
+#include "core/foo.h"
+
+int main(int argc, const char* argv[]) {
+  foo::foo();
+  return 0;
+}
---
+++ engine/core/foo.cc
@@ -0,0 +1,13 @@
+// Copyright 2023 Berke Umut Biricik All Rights Reserved
+
+#include "core/foo.h"
+
+#include <iostream>
+
+namespace foo {
+
+void foo() {
+  std::cout << "Hello, World!\n";
+};
+
+}  // namespace foo
