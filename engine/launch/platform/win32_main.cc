a4840ba57f90727ea0fc98eb31635885db805840
Berke Biricik <berkeumtbiricik@gmail.com>
2024-01-02 20:26:59 +0300

    remove: win32 executable and update: editor screenshot.

--- engine/launch/platform/win32_main.cc
+++ engine/launch/platform/win32_main.cc
@@ -6,13 +6,6 @@ namespace eve {
 extern int GuardedMain(CommandLineArguments args);
 }
 
-#ifdef _DEBUG
 int main(int argc, char* argv[]) {
   return eve::GuardedMain({argc, argv});
 }
-#else
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-                   LPSTR lpCmdLine, int nShowCmd) {
-  return eve::GuardedMain({__argc, __argv});
-}
-#endif
--- engine/launch/platform/win32_main.h
+++ engine/launch/platform/win32_main.h
@@ -4,9 +4,4 @@
 
 #include <Windows.h>
 
-#ifdef _DEBUG
 int main(int argc, char* argv[]);
-#else
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-                   LPSTR lpCmdLine, int nShowCmd);
-#endif
